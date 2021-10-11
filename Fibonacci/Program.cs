f158a544eb403e4b506dcba362215f6ef604486c
Lidiia <l.potryasaeva@gmail.com>
2021-10-11 10:55:32 +0300

    fibonacci task

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main()
+        {
+            //(*) Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом. 
+
+            Console.WriteLine("Enter integer number for FibonacciSeries!");
+            int num = Convert.ToInt32(Console.ReadLine());
+            var result = fibonacci(num);
+            Console.WriteLine($"Fibonacci number {result}");
+            Console.ReadLine();
+
+
+            static int fibonacci(int n)
+            {
+                if (n == 1 || n == 2)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return fibonacci(n - 1) + fibonacci(n - 2);
+                }
+            }
+        }
+    }
+}
